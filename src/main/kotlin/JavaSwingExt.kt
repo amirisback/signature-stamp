d6e41e67fafd1e32f894ac9f6116513f005efb58
amirisback <faisalamircs@gmail.com>
2026-04-15 21:43:47 +0700

    feat: implement tool window UI and signature stamp insertion functionality

--- src/main/kotlin/JavaSwingExt.kt
+++ src/main/kotlin/JavaSwingExt.kt
@@ -1,6 +1,7 @@
 package io.github.amirisback
 
 import com.intellij.ui.components.JBLabel
+import java.awt.Component
 import javax.swing.JButton
 
 fun varJLabel(text: String, params: Any? = null): JBLabel {
@@ -9,4 +10,8 @@ fun varJLabel(text: String, params: Any? = null): JBLabel {
 
 fun varJButton(text: String, params: Any? = null): JButton {
     return JButton(MyMessageBundle.message(text, params))
+}
+
+fun emptyVerticalSpace(dimen: Int): Component {
+    return javax.swing.Box.createRigidArea(java.awt.Dimension(0, dimen))
 }
--- src/main/kotlin/MyToolWindowProp.kt
+++ src/main/kotlin/MyToolWindowProp.kt
@@ -1,2 +1,41 @@
 package io.github.amirisback
 
+import com.intellij.ui.components.JBLabel
+import javax.swing.JButton
+
+fun propTextTitle(): JBLabel {
+    val text = "Welcome to Signature Stamp"
+
+    return JBLabel(text).apply {
+        font = font.deriveFont(18f)
+        alignmentX = java.awt.Component.LEFT_ALIGNMENT
+    }
+}
+
+fun propTextDescription(): JBLabel {
+    val text = """
+            <html>
+                <ul>
+                    <li>This plugin allows you to easily add a signature stamp to your code files.</li>
+                    <li>You can customize the stamp with your name, date, and a custom message.</li>
+                    <li>To get started, simply click the button below to generate your signature stamp.</li>
+                </ul>
+            </html>
+        """.trimIndent()
+
+    return JBLabel(text).apply {
+        font = font.deriveFont(14f)
+        alignmentX = java.awt.Component.LEFT_ALIGNMENT
+    }
+}
+
+fun propButtonAddStamp(action: () -> Unit): JButton {
+    val text = "Add Stamp"
+
+    return JButton(text).apply {
+        // alignmentX = java.awt.Component.LEFT_ALIGNMENT
+        addActionListener {
+            action()
+        }
+    }
+}
